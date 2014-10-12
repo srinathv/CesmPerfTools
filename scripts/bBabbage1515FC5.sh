7a65b99334ac6cc79f283848d030a0a8662951cb
Srinath Vadlamani <srinathv@ucar.edu>
2014-10-11 23:22:00 -0600

    changed to impi 5.0.1

--- scripts/bBabbage1515FC5.sh
+++ scripts/bBabbage1515FC5.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-/chos/global/u1/v/vadlaman/cesm1_3_xeon_phi_branch/scripts/create_newcase -case $1 -compiler intel15 -mpi impi5.0.up1 -compset FC5 -res ne16_ne16 -mach babbage
+/chos/global/u1/v/vadlaman/cesm1_3_xeon_phi_branch/scripts/create_newcase -case $1 -compiler intel15 -mpi impi5.0.1 -compset FC5 -res ne16_ne16 -mach babbage
