c0cfb3228894808f4725116b7783bea8ef6ba82e
Srinath Vadlamani <srinathv@ucar.edu>
2014-12-09 14:35:34 -0700

    labels and title

--- tools/calcAvgHommeTiming.py
+++ tools/calcAvgHommeTiming.py
@@ -2,7 +2,7 @@
 
 import sys,os
 import numpy as np
-import pylab as py
+import matplotlib.pyplot as py
 sys.path.append('../modules')
 try:
   import cesmperftiming as cpt
@@ -11,7 +11,7 @@ except:
 
 
 def main(argv):
-    thisPath = os.path.abspath(".")
+  thisPath = os.path.abspath(".")
   try:
     newPath=os.path.join(thisPath,argv[1])
     os.chdir(os.path.dirname(newPath))
@@ -47,10 +47,12 @@ def main(argv):
     numBins=50
 
   #n,bins,patches=py.hist(primRunArray,bins=50)
-  n,bins,patches=py.hist(primRunArray,bins=numBins)
-  py.xlabel="prim_run [sec]"
-  py.ylabel="Number of Homme trials"
-  py.title= thisDir + " NE=3, 1 mpi rank at full device thread use"
+  fig1=py.figure(1)
+  ax = fig1.add_subplot(1,1,1,)
+  n,bins,patches=ax.hist(primRunArray,bins=numBins)
+  py.xlabel("prim_run [sec]")
+  py.ylabel("Number of Homme trials")
+  py.title(thisDir + " NE=3, 1 mpi rank at full device thread use\n Gaussian like")
   py.show()
 
 
